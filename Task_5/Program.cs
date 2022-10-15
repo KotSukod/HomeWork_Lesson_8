c54cd8670015a4f2b8007d27477461182ec13fa0
KotSukod <XaoS.07@mail.ru>
2022-10-15 23:23:40 +1000

    Задание 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+int[,] array = new int[4,4];
+int i = 0;
+int j = 0;
+int number = 1;
+for (j = 0; j < array.GetLength(1); j++)
+{
+    array[i,j] = number;
+    number++;
+}
+j =j - 1;
+for(i = 1; i < array.GetLength(0); i++)
+{
+    array[i,j] = number;
+    number++;
+}
+i = i-1;
+for(j = j - 1;j >= 0; j--)
+{
+    array[i,j] = number;
+    number++;
+}
+j =j +1;
+for (i = i-1; i >= 1; i--)
+{
+        array[i,j] = number;      
+        number++;
+}
+i = i + 1;
+for(j=j +1; j < array.GetLength(1); j++)
+{
+    if (array[i,j] == 0 )
+    {
+        array[i,j] = number;
+        number++;
+    }
+    else if (array[i,j] !=0) break;
+}   
+j = j-1;
+for (i = i+1; i< array.GetLength(0); i++)
+{
+    if (array[i,j] == 0 )
+    {
+        array[i,j] = number;
+        number++;
+    }
+    else if (array[i,j] !=0) break;
+}
+i = i-1;
+for (j = j-1; j< array.GetLength(1); j--)
+{
+    if (array[i,j] == 0 )
+    {
+        array[i,j] = number;
+        number++;
+    }
+    else if (array[i,j] !=0) break;
+}
+i = 0;
+while (i < array.GetLength(0))
+{
+    j = 0;
+    while(j < array.GetLength(1))
+    {
+        Console.Write($"{array[i,j]} ");
+        j++;
+    }
+    Console.WriteLine();
+    i++;
+}
